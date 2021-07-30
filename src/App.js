eebeb11c53c6c721f264514d10f7149fc71e6a7d
rkh-leung <leptanian@gmail.com>
2021-07-30 22:13:34 +1200

    add: fcc test cdn, font awesome cdn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <textarea id={'preview'} />
       </header>
     </div>
   );

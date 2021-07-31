7fdea3ce9ab3b34eb12056e150f8f14ad3dd237c
rkh-leung <leptanian@gmail.com>
2021-07-31 15:10:26 +1200

    add: proper value to display user input in preview

--- src/components/markdown-preview.jsx
+++ src/components/markdown-preview.jsx
@@ -1,36 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import marked_1 from 'marked/lib/marked.esm'
 
 export const MarkdownPreview = ({ markdown }) => {
   const [input, setInput] = useState(markdown)
   const handleChange = (e) => setInput(e.target.value)
-  const opts = {
-    baseUrl: null,
-    breaks: false,
-    extensions: null,
+  const markedOpts = {
+    sanitize: true,
     gfm: true,
-    headerIds: true,
-    headerPrefix: '',
-    highlight: null,
-    langPrefix: 'language-',
-    mangle: true,
-    pedantic: false,
-    sanitize: false,
-    sanitizer: null,
-    silent: false,
-    smartLists: false,
-    smartypants: false,
-    tokenizer: null,
-    walkTokens: null,
-    xhtml: false,
+    breaks: true,
   }
+
   return (
     <>
       <textarea id={'editor'} value={input} onChange={handleChange} />
       <div
         id={'preview'}
         dangerouslySetInnerHTML={{
-          __html: marked_1(markdown, opts),
+          __html: marked_1(input, markedOpts),
         }}
       />
     </>
